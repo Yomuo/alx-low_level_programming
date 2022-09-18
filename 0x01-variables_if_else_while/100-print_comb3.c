3a559b0ad2b9381f4f5b476af4f5c5868e029bbb
Yomuo <sntlmay@gmail.com>
2022-09-18 01:34:28 -0700

    asdf

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n1;
+	int n2;
+
+	for (n1 = '0'; n1 <= '9'; n1++)
+	{
+		for (n2 = '0'; n2 <= '9'; n2++)
+		{
+			if (n2 > n1)
+			{
+				putchar(n1);
+				putchar(n2);
+			}
+			if (n2 > n1 && n1 != '8')
+			{
+			putchar(',');
+			putchar(' ');
+			}
+			if (n2 == n1)
+			{
+
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
