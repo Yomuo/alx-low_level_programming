dcc11c9b891d601e2c07353ac9e6b7d42ff62055
Ahmed <sntlmay@gmail.com>
2022-12-13 18:09:22 -0800

    commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,12 +16,12 @@ int main(void)
 		{
 			for (c = '2'; c <= '9'; c++)
 			{
-				if (c > b && b > a )
+				if (c > b && b > a)
 				{
 					putchar(a);
 					putchar(b);
 					putchar(c);
-					if(!(a == '7' && b == '8' && c == '9'))
+					if (!(a == '7' && b == '8' && c == '9'))
 					{
 						putchar(',');
 						putchar(' ');
