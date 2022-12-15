c9261a082c869b3d18a6621f17e291442f91bc5a
Ahmed <sntlmay@gmail.com>
2022-12-15 12:02:03 -0800

    asdf

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main -function prints alphabet
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar(10);
+	return (0);
+}
