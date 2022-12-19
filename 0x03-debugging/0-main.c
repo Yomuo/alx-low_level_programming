45baa3d29da977f2bba9cec97b3129088423bfed
Ahmed <sntlmay@gmail.com>
2022-12-18 17:21:59 -0800

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - function are testing the positive_or_negative()
+ * Return: return 0 if gives the correct output
+ */
+int main(void)
+{
+	int n = 98;
+
+	positive_or_negative(n);
+	if (postitive_or_negative(n) == 0)
+	{
+	return (0);
+	}
+}
